0f6b610f7f38c75804674e820e8dba7541128ebd
Megha Harnekar <megha.harnekar@i25743-is2.octanner.com>
2015-09-16 17:03:10 -0600

    react tutorial added

---
+++ public/tutorial1.js
@@ -0,0 +1,68 @@
+var CommentBox = React.createClass({
+  render: function() {
+    
+    return (
+      <div className="commentBox">
+      <h1> Comments</h1>
+     <CommentList data={this.props.data} />
+      <CommentForm />
+      </div>
+    );
+  }
+});
+
+React.render(
+   <CommentBox url="comments.json" />,
+  document.getElementById('content')
+);
+
+//tutorial.js
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return(
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+      <div className="commentForm">
+      Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function(){
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return(
+      <div className="comment">
+      <h2 className "commentAuthor">
+      {this.props.author}
+      </h2>
+       <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
+});
+
+
+// tutorial8.js
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+
